d22375fc4fd874e327710b4f30feeef3a8b63a57
robinmarlow <robin.marlow@gmail.com>
2023-10-06 15:33:56 +0100

    Update import-data.R

--- import-data.R
+++ import-data.R
@@ -1,2 +1,7 @@
 library(tidyverse)
-write_file(as.character(now()), "test.txt")
+SECRET_THING <- Sys.getenv("SECRET_THING")
+SECOND_SECRET <- Sys.getenv("SECOND_SECRET")
+
+output<-paste(as.character(now()),"\n",SECRET_THING,"\n",SECOND_SECRET)
+
+write_file(output, "test.txt")
